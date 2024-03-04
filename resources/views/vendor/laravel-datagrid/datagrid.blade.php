630aa90da80c2a101120c2f2152f3baa48d62745
Daniel Werner <vernerd@gmail.com>
2024-03-04 19:31:43 +0100

    Mass actions wip

--- resources/views/vendor/laravel-datagrid/datagrid.blade.php
+++ resources/views/vendor/laravel-datagrid/datagrid.blade.php
@@ -2,4 +2,5 @@
     base-url={{$baseUrl}}
     :columns="{{json_encode($columns)}}"
     :rows="{{json_encode($rows)}}"
+    :mass-actions="{{json_encode([['action' => 'massDelete', 'label' => 'Delete']])}}"
 ></data-grid>
